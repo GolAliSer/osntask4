7ce174b4c645470cc5411f783dd2b1579194953d
Alina <asgolubeva_1@edu.hse.ru>
2022-10-14 17:15:49 +0300

    commit

--- 2.py
+++ 2.py
@@ -1,47 +1,47 @@
 #задание 2
 while True:
     try:
-        N1 = int(input("Введите количество элементов массива A: "))
-        N2 = int(input("Введите количество элементов массива B: "))
+        N1 = int(input("Введите количество элементов массива A: "))#вводим размерность массива А
+        N2 = int(input("Введите количество элементов массива B: "))#вводим размерность массива В
         break
-    except ValueError:
+    except ValueError:#проверяем на ошибку ввода
         print("Вы ввели не целое число. Попробуйте снова: ")
-A = [0] * N1
-B = [0] * N2
+A = [0] * N1#создаем массив А
+B = [0] * N2#создаем массив В
 while True:
     try:
-        sp = int(input("Введите '1' для самостоятельного ввода массивов, либо введите любое другое число: "))
+        sp = int(input("Введите '1' для самостоятельного ввода массивов, либо введите любое другое число: "))#
         break
-    except ValueError:
+    except ValueError:#проверяем на ошибку ввода
         print("Вы ввели не целое число. Попробуйте снова: ")
-if sp == 1:
+if sp == 1:#заполнение массива с клавиатуры
     print("Введите", N1, "элементов для массива A:")
-    for k in range(N1):
+    for k in range(N1):#заполняем массив А
         while True:
             try:
                 print("A[", k, "] = ", end=" ")
                 A[k] = float(input())
                 break
-            except ValueError:
+            except ValueError:#проверяем на ошибку ввода
                 print("Вы ввели не число. Попробуйте снова: ")
     print("Введите", N2, "элементов для массива B:")
-    for l in range(N2):
+    for l in range(N2):#заполняем массив В
         while True:
             try:
                 print("B[", l, "] = ", end=" ")
                 B[l] = float(input())
                 break
-            except ValueError:
+            except ValueError:#проверяем на ошибку ввода
                 print("Вы ввели не число. Попробуйте снова: ")
-else:
+else:#заполнение массива случайными числами
     from random import random
     for i in range(N1):
         A[i] = round(random() * 10, 1)
     for z in range(N2):
         B[z] = round(random() * 10, 1)
 print("Входые массивы:", A, B)
-print("Общие элементы массивов:", end="")
-for o in range(N1):
+print("Общие элементы массивов: ", end="")
+for o in range(N1):#перебором находим общие элементы массивов
     for u in range(N2):
         if A[o] == B[u]:
             print(A[o], end = " ")
--- бонусное.py
+++ бонусное.py
@@ -1,30 +1,30 @@
 #бонусное задание
 while True:
     try:
-        N = int(input("Введите количество элементов массива A: "))
+        N = int(input("Введите количество элементов массива A: "))#вводим размерность
         break
-    except ValueError:
+    except ValueError:#проверяем на ошибку ввода
         print("Вы ввели не целое число. Попробуйте снова: ")
-A = [0] * N
+A = [0] * N#создаем массив
 from random import randint
-for i in range(N):
+for i in range(N):#заполняем массив случайными числами
         A[i] = randint(0, 99)
 print("Входной массив:", A)
 while True:
     try:
-        Delta = int(input("Введите Delta: "))
+        Delta = int(input("Введите Delta: "))#вводим значение delta
         break
-    except ValueError:
+    except ValueError:#проверяем на ошибку ввода
         print("Вы ввели не целое число. Попробуйте снова: ")
-min = 100
-nr = 0
-for n in range(N):
+min = 100#минимальное число
+nr = 0#номер минимального числа
+for n in range(N):#перебором находим минимальное число
     if A[n] < min:
         min = A[n]
         nr = n
 print("Минимальный элемент массива:", min)
-k = 0
-for m in range(N):
+k = 0#счетчик количества элементов
+for m in range(N):#
     if A[m] - min == Delta:
         k += 1
 print("Результат:", k)
